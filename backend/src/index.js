0e39f8ac4c4dd7cc76f242262dc28b50cd231e04
unknown <henrique.dcti@gmail.com>
2020-07-15 18:19:55 -0300

    Front-end do app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
-const cors = requrie('cors');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors());
 
 app.listen(3333);
