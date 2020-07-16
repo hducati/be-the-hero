4f95ce6df8303acd4db244acef303b92dfc38b76
unknown <henrique.dcti@gmail.com>
2020-07-15 22:50:27 -0300

    Mobile do app

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Incidents from './pages/Incidents'
+import Detail from './pages/Detail'
+import React from 'react';
+
+const AppStack = createStackNavigator();
+
+export default function Routes() {
+    return(
+        <NavigationContainer>
+            <AppStack.Navigator screenOptions={{ headerShown: false}}>
+                <AppStack.Screen name="Incidents" component={Incidents}/>
+                <AppStack.Screen name="Detail" component={Detail}/>
+            </AppStack.Navigator>
+        </NavigationContainer>
+    )
+}
